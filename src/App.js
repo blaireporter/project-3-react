9ceb08f9fed7bceeff55b40fcbd91826cce25cf1
blaire <blaire.e.porter@gmail.com>
2021-03-05 17:41:53 -0500

    created files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [fortune, setFortune] = useState('');
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    // getResults();
+  },[])
+
+  const getResults = () => {
+    fetch('https://8ball.delegator.com/magic/JSON/""')
+      .then(response => {
+        return response.json()
+      })
+      .then(jsonResponse => {
+        console.log("jsonResponse", jsonResponse)
+        setFortune(jsonResponse.magic.answer)
+      })
+
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div>
+        <h1>magic 8 ball time</h1>
+        <button onClick={getResults} className={isActive ? "active" : ""}>click to get fortune</button> 
+        {fortune && <p className="">{fortune}</p>}
+        <p>type your question above</p>
+      </div>
+    </main>
+  )
 }
 
 export default App;
+
+// return (
+//     <div>
+//       <h1>magic 8 ball time</h1>
+//       <button onClick={() => setFortune(!fortune)} >click to get fortune</button>
+//       {
+//         fortune === true
+//           ? <p>{fortune}</p>
+//           : <p>type your question above</p>
+//       }
+//     </div>
+//   )
+
